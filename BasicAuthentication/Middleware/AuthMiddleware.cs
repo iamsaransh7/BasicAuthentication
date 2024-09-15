38c7da844d0b2ba872be9a2b9c7792c2dbf16182
Saransh Upadhyay <iamsaransh7@gmail.com>
2024-09-15 19:33:47 +0530

    AuthMiddleware Added

--- BasicAuthentication/Middleware/AuthMiddleware.cs
+++ BasicAuthentication/Middleware/AuthMiddleware.cs
@@ -1,6 +1,34 @@
-﻿namespace BasicAuthentication.Middleware
+﻿
+using System.Text;
+
+namespace BasicAuthentication.Middleware
 {
-    public class AuthMiddleware
+    public class AuthMiddleware : IMiddleware
     {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            var headers = context.Request.Headers["Authorization"].ToString();
+            var creds = headers.Substring(6);
+            var encodedString = Encoding.UTF8.GetString(Convert.FromBase64String(creds));
+            string[] credentialString = encodedString.Split(',');
+            string userName = credentialString[0];
+            string password = credentialString[1];
+
+            if (userName != "saransh" || password != "Test@123")
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await next(context);
+        }
     }
 }
